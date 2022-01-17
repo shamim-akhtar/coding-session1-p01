4c2e477cdbcce2e123596b022f7d4df9a299566b
Shamim Akhtar <shamim.akhtar@gmail.com>
2022-01-17 11:40:25 +0800

    I have completed refactoring.
    1. All classes are now in separate files. The filename is same as my class name.
    2. I have created a new folder called TPC which contains all my third-person camera classes .
    3 I have added a namespace TPC for all TPC camera implementations.

---
+++ UnityProject/Assets/Scripts/GameConstants.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public static class GameConstants
+{
+  public static Vector3 CameraAngleOffset { get; set; }
+  public static Vector3 CameraPositionOffset { get; set; }
+  public static float Damping { get; set; }
+  //public static float RotationSpeed { get; set; }
+  //public static float MinPitch { get; set; }
+  //public static float MaxPitch { get; set; }
+}
